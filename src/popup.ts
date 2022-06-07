b8f4ad55d6d836f23531d84e481cf3c7815e37bc
deadManAlive <alfarrakhan@gmail.com>
2022-06-07 13:53:42 +0700

    base implementation

--- src/popup.ts
+++ src/popup.ts
@@ -2,6 +2,21 @@
 
 let runButton = <HTMLElement>document.getElementById("runAgent");
 
+//options view
+chrome.storage.sync.get(["codeQuery", "termQuery"], ({codeQuery, termQuery}) => {
+    let cview = <HTMLElement>document.getElementById("cqueries");
+    let tview = <HTMLElement>document.getElementById("tqueries");
+
+    cview.append(codeQuery.join("\n"));
+    tview.append(termQuery.join("\n"));
+});
+
+
+function mainproc(codes: string[], terms: number[]) {
+    let c = document.getElementsByClassName("box");
+    console.log(c);
+}
+
 runButton.addEventListener("click", async () =>{
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
@@ -9,6 +24,7 @@ runButton.addEventListener("click", async () =>{
         target: {tabId: <number>tab.id},
         func: () => {
             chrome.storage.sync.get(["codeQuery", "termQuery"], ({codeQuery, termQuery}) => {
+                mainproc(codeQuery, termQuery);
                 console.log("queries: code -> ", codeQuery, " term --> ", termQuery);
             });
         }
